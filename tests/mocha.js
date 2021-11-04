d8e5263ab24666a919ddd2824f271040858eb000
Andy <admazzola@gmail.com>
2021-11-04 15:59:48 -0400

    fixing test config

--- tests/mocha.js
+++ tests/mocha.js
@@ -14,12 +14,19 @@ describe("Vibegraph Data Collector", function() {
         let web3 = new Web3( web3Config.web3provider  )
 
         let vibegraphConfig = {
-            contractAddress:"0xab89a7742cb10e7bce98540fd05c7d731839cf9f",
-            startBlock:1316824,
+            contracts:[{
+                //goerli
+                 address:"0xab89a7742cb10e7bce98540fd05c7d731839cf9f",
+                 startBlock:1316824,
+                 type:'ERC20'
+
+            }],
+            
             dbName:"vibegraph_test",
-            contractType: 'ERC20', 
-             courseBlockGap: 10000,
-             logging:true,
+            indexRate: 10*1000,
+            courseBlockGap: 8000,
+            fineBlockGap: 20,
+            logging:true,
         }
 
         let vibegraph = new VibeGraph()
