332f00133b2ed79ff764056c09ebe3de8a993453
Admazzola <admazzola@gmail.com>
2023-02-16 15:43:39 -0500

    indexer can compute the token id

--- indexers/IndexerENSRegistry.ts
+++ indexers/IndexerENSRegistry.ts
@@ -1,6 +1,7 @@
 
 //const VibegraphIndexer = require('./VibegraphIndexer')
- import { ContractEvent } from '../src'
+ import { ethers, BigNumber } from 'ethers'
+import { ContractEvent } from '../src'
 import VibegraphIndexer from './VibegraphIndexer'
 
 module.exports =  class IndexerENSRegistry extends VibegraphIndexer {
@@ -8,6 +9,18 @@ module.exports =  class IndexerENSRegistry extends VibegraphIndexer {
    
 
     async onEventEmitted(event:ContractEvent){
+
+        console.log('got emitted event ', event )
+ 
+        let eventArgs:any = event.args 
+        let registeredName = eventArgs[0]
+
+        const labelHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(registeredName))
+        const tokenId = BigNumber.from(labelHash).toString()
+        
+
+        //this works ! 
+        console.log({tokenId})
 
        
     }
